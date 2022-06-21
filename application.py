00ec6c5b94fcba0405c1b5fa8c0c244bcbaadafd
shruthi-epiroc <106649846+shruthi-epiroc@users.noreply.github.com>
2022-06-21 15:36:27 +0530

    Update application.py

--- application.py
+++ application.py
@@ -81,5 +81,3 @@ def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'],
                                filename)
 
-if __name__ == "__main__":
-    app.run(threaded=False)
